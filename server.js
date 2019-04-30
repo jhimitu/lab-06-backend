d05c15f81355e351a248287fd0fe4f8e1a081743
Jhia Turner <jhimitu@gmail.com>
2019-04-30 10:53:28 -0700

    create instance of data from client query

--- server.js
+++ server.js
@@ -9,7 +9,14 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('./client'));
 
 app.get('/location', (request, response) => {
-  response.send('yes');
+  const data = require('./data/geo.json');
+  let search_query = request.query.data;
+  let formatted_query = data.results[0].formatted_address;
+  let latitude = data.results[0].geometry.location.lat;
+  let longitude = data.results[0].geometry.location.lng;
+  let location = new Location(search_query, formatted_query, latitude, longitude);
+  console.log(location);
+  response.send(location);
 });
 
 app.listen(PORT, () => {
