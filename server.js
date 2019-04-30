2464632a87de1d594006ae3a122934fabc20f550
Jhia Turner <jhimitu@gmail.com>
2019-04-30 09:35:04 -0700

    set up basic server

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./client'));
+
+app.get('/location', (request, response) => {
+  response.send('yes');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
