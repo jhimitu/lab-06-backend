e9bfd258837734320ee2a7fe1c3d5f8032cd7513
Jhia Turner <jhimitu@gmail.com>
2019-04-30 11:28:37 -0700

    add error handling

--- server.js
+++ server.js
@@ -9,23 +9,30 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('./client'));
 
 app.get('/location', (request, response) => {
-  const data = require('./data/geo.json');
-  let search_query = request.query.data;
-  let formatted_query = data.results[0].formatted_address;
-  let latitude = data.results[0].geometry.location.lat;
-  let longitude = data.results[0].geometry.location.lng;
-  let location = new Location(search_query, formatted_query, latitude, longitude);
-  console.log(location);
-  response.send(location);
+  try {
+    const data = require('./data/geo.json');
+    let search_query = request.query.data;
+    let formatted_query = data.results[0].formatted_address;
+    let latitude = data.results[0].geometry.location.lat;
+    let longitude = data.results[0].geometry.location.lng;
+    let location = new Location(search_query, formatted_query, latitude, longitude);
+    console.log(location);
+    response.send(location);
+  } catch (error) {
+    response.status(500).send('Sorry, something went wrong');
+  }
 });
 
 app.get('/weather', (request, response) => {
-  const data = require('./data/darksky.json');
-  let forecast = data.currently.summary;
-  let time = new Date(data.currently.time).toString().slice(0, 15);
-  //new Date(obj.time).toString().slice(0, 15);
-  let weather = new Weather(forecast, time);
-  response.send(weather);
+  try {
+    const data = require('./data/darksky.json');
+    let forecast = data.currently.summary;
+    let time = new Date(data.currently.time).toString().slice(0, 15);
+    let weather = new Weather(forecast, time);
+    response.send(weather);
+  } catch (error) {
+    response.status(500).send('Sorry, something went wrong');
+  }
 });
 
 app.listen(PORT, () => {
